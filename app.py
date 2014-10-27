85d5947035a878f5112c02821de60c89a9650070
briancarlo <briancarlo@users.noreply.github.com>
2014-10-27 10:46:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import shelve
 from subprocess import check_output
 import flask
-from flask import request
-from flask import render_template
+from flask import request, render_template
 from os import environ
 
 app = flask.Flask(__name__)
@@ -11,44 +10,44 @@ app.debug = True
 
 db = shelve.open("shorten.db")
 
+import random                                           # helps set random path if user doesn't supply a shortpath
+from urlparse import urlparse                           # helps check if "http" hasn't been added when user inputs URL
+import httplib2                                         # for URL verification
 
-# This sets up the server-generated random hash if user doesn't supply a shortpath
-import random
-possible = range(48,58) + range(65,91) + range(97,123)
-
-
-# This module helps us check to see if "http" hasn't been added when user inputs URL
-from urlparse import urlparse
+possible = range(48,58) + range(65,91) + range(97,123)  # range of possible ord for alphanumeric random path
 
 
-# Renders our landing page from the templates folder 
-@app.route('/')
+@app.route('/')                                         # Renders our landing page from the templates folder                                          
 def root():
-    return render_template('test_copy.html')
+    return render_template('test2.html')
 
 @app.before_request
 def validate():
     if request.path == '/shorts':
-        shortpath = str(request.form.get('shortpath'))
-        if db.get(shortpath):
-            return 'Short path already taken', 415
-        elif not shortpath.isalpha():
-            return 'Please use only letters for shortpath', 404 
+        shortpath = str(request.form.get('shortpath')).strip()
+        if shortpath == "":
+            pass
+        else:
+            if db.get(shortpath):
+                return 'Short path already taken', 415
+            elif not shortpath.isalpha():
+                return 'Unsupported Media Type', 415 
 
+        inputURL = str(request.form.get('url')).lower().strip()
+        if inputURL == None or inputURL == "":
+            return 'Please enter a URL'
+
+#        h = httplib2.Http()
+#        response = h.request(inputURL, 'HEAD')
+#        if int(response[0]['status']) < 400:
+#            return 'Please enter a valid URL'
         
-       # inputURL = request.form.get('url')
-       # if inputURL:
-       #     return 'THERE IS A URL'
+             
 
-
-
-# Where the magic happens
-@app.route('/shorts', methods=['PUT', 'POST'])
+@app.route('/shorts', methods=['PUT', 'POST'])          # Where the magic happens
 def shorts_put():
 
-    app.logger.debug("whatup")
-
-    inputURL = request.form.get('url', '')              # grabs value from 'url' key passed in http POST
+    inputURL = str(request.form.get('url', '')).lower() # grabs value from 'url' key passed in http POST
 
     parsed = urlparse(inputURL)                         # creates urlparse object
     if parsed.scheme == "":                             # if the scheme doesn't exist ...
@@ -57,23 +56,22 @@ def shorts_put():
     # grab the value from the 'shortpath' key passed in http POST
     shortpath = str(request.form.get('shortpath', ''))  # for some reason str() matters here ...
  
-    # this block triggers if the user sent only a URL and NO shortpath
-    if not shortpath:
+    if not shortpath:                                   # this block triggers if the user sent only a URL and NO shortpath
         if db.get(inputURL):                            # check to see if the URL is already in the DB
             shortpath = db[inputURL]                    # yeah? set it to the shortpath variable
         else:
-            myHash = ""                                 # no? create a hash of seven random chars
+            myPath = ""                                 # no? create myPath of seven random chars
             for i in range(7):
-                myHash = myHash + chr(random.choice(possible))
+                myPath = myPath + chr(random.choice(possible))
        
-            while db.get(myHash):                       # if that hash somehow exists, create more until a unique one is created
-                myHash = ""
+            while db.get(myPath):                       # if myPath somehow exists, create more until a unique one is created
+                myPath = ""
                 for i in range(7):
-                    myHash = myHash + chr(random.choice(possible))  
+                    myPath = myPath + chr(random.choice(possible))  
 
-            db[inputURL] = myHash                       # add the URL to the dict as a key; now any future attempts to ..  
+            db[inputURL] = myPath                       # add the URL to the dict as a key; now any future attempts to ..  
             db[myHash] = inputURL                       # ... shorten the same URL (without a specified shortpath) will ...
-            shortpath = myHash                          # ... return the same hash. And, of course, add the hash key to the DB like normal.
+            shortpath = myPath                          # ... return the same hash. And, of course, add the myPath key to the DB like normal.
     
     # if URL is sent WITH a shortpath, just add that key/value pair to the DB
     else:
@@ -82,10 +80,11 @@ def shorts_put():
 
 @app.route('/shorts/<shortpath>', methods=['GET'])
 def shorts_shortpath(shortpath):   
-    url = db[str(shortpath)]
-    # return flask.redirect(url)
-    return flask.redirect(db[str(shortpath)])
-
+    if db.get(str(shortpath)):
+        url = db[str(shortpath)]
+        return flask.redirect(db[str(shortpath)])
+    else:
+        return render_template('404.html')        
     
 if __name__ == "__main__":
     app.run(port=int(environ['FLASK_PORT']))
